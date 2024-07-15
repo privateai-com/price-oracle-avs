1bfa11247ce72441b442cce70285b754d611efdc
rblockchaindeveloper <rblockchaindeveloper@gmail.com>
2024-07-15 19:17:44 +0530

    added diabetes code

--- AVS_WebAPI/src/validator.service.js
+++ AVS_WebAPI/src/validator.service.js
@@ -2,13 +2,22 @@ require('dotenv').config();
 const dalService = require("./dal.service");
 const oracleService = require("./oracle.service");
 
-async function validate(proofOfTask) {
-
+async function validate(proofOfTask){
   try {
       const taskResult = await dalService.getIPfsTask(proofOfTask);
-      var data = await axios.post()
+          const res = await axios.post("https://ai.privateai.com:8002/predict_diabetes_api",{
+            "pregnancies": taskResult.input[0],
+            "glucose": taskResult.input[1],
+            "bloodpressure": taskResult.input[2],
+            "skinthickness": taskResult.input[3],
+            "insulin": taskResult.input[4],
+            "bmi": taskResult.input[5],
+            "dpf": taskResult.input[6],
+            "age": taskResult.input[7]
+        })
+          console.log(res.data);
       let isApproved = true;
-      if (taskResult.price > upperBound || taskResult.price < lowerBound) {
+      if (taskResult.answer == res.data.answer) {
         isApproved = false;
       }
       return isApproved;
--- Task_Performer/src/llmApi.service.js
+++ Task_Performer/src/llmApi.service.js
@@ -6,13 +6,18 @@ const initialPrompt = "You are an assistant who is experienced in the healthcare
   async function getLLMResponse(prompt){
     var res = null;
     try{
-      res = await axios.post("https://ai.privateai.com:5000/v1/completions",{
-        "model": "openbiollm-llama3-8b",
-        "prompt": initialPrompt + "Medical Question: "+prompt+"? Medical Answer:",
-        "temperature": 0.1
+      res = await axios.post("https://ai.privateai.com:8002/predict_diabetes_api",{
+        "pregnancies": prompt[0],
+        "glucose": prompt[1],
+        "bloodpressure": prompt[2],
+        "skinthickness": prompt[3],
+        "insulin": prompt[4],
+        "bmi": prompt[5],
+        "dpf": prompt[6],
+        "age": prompt[7]
     })
-      console.log(res.data.choices[0].text);
-      return res.data.choices[0].text
+      console.log(res.data);
+      return res.data
     }catch(e){
       console.log(e);
       return 0;
